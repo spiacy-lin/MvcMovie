35e40fe060fccf9a49d69fb54b73a3e3f71bae12
Andrzej Malik <andrzejkom2016@gmail.com>
2020-06-15 08:38:14 +0200

    title filter

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 
 namespace MvcMovie.Models
@@ -10,11 +11,15 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int Id { get; set; }
+        [StringLength(60,MinimumLength =3)]
+        [Required]
         public string Title { get; set; }
 
+        [Display(Name ="Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
